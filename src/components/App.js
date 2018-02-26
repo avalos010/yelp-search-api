deeff312247407665122c1f8fda8e5a18001391e
JLuis Avalos <luizavalos40@gmail.com>
2018-02-25 19:11:14 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component {
  
   search = (e) => {
     e.preventDefault();
-    fetch('http://localhost:3002/query',{
+    fetch('https://yelp-server-api.herokuapp.com/query',{
     method:'POST',
     headers: {
       'Accept': 'application/json',
